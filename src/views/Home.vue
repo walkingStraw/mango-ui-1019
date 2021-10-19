0562cb5c6be77be0cd83fd14f98437c27305ca94
lizhibin <1030379170@qq.com>
2021-10-19 11:12:41 +0800

    【新增】安装并测试axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="page">
     <h2>Home Page</h2>
+    <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    testAxios() {
+      axios.get('http://localhost:8080/hello').then(
+        res => {alert(res.data)}
+      )
+    }
+  }
 }
 </script>
